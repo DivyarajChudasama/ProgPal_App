8c6a81e29cf0936060b4b24c4c10e92e783dc328
= <=>
2024-01-31 21:26:37 +0530

    intro_screens_rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:progpal/screens/introduction_screens.dart';
+import 'package:progpal/screens/intro_screens.dart';
 
 void main() {
   runApp(const MyApp());
