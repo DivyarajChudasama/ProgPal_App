a6c433c5e8fdb003ab057e9073bd096959cfc3ca
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2024-02-15 09:30:10 +0530

    programs page

--- lib/screens/java/programs/basic.dart
+++ lib/screens/java/programs/basic.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 
 class BasicPage extends StatefulWidget {
-  const BasicPage({super.key});
+  const BasicPage({Key? key}) : super(key: key);
 
   @override
   State<BasicPage> createState() => _BasicPageState();
@@ -10,6 +12,124 @@ class BasicPage extends StatefulWidget {
 class _BasicPageState extends State<BasicPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Basic Programs'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Hello World'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HelloWorld()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('If-Else'),
+            onTap: () {
+              // Add functionality to navigate to the If-Else program page
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  final String helloWorldCode = '''
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!!");
+    }
+}
+''';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            LineNumbers(),
+            const SizedBox(width: 16.0),
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(color: Colors.grey.shade300),
+                    ),
+                    child: Text(
+                      helloWorldCode,
+                      style: TextStyle(
+                        fontFamily: 'Courier New',
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 8.0,
+                    right: 8.0,
+                    child: IconButton(
+                      icon: Icon(Icons.copy),
+                      onPressed: () {
+                        Clipboard.setData(ClipboardData(text: helloWorldCode));
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Code copied to clipboard')),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LineNumbers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(8.0),
+          bottomLeft: Radius.circular(8.0),
+        ),
+        border: Border.all(color: Colors.grey.shade300),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            '1\n2\n3\n4\n5\n6\n7\n8\n9\n10',
+            style: TextStyle(
+              fontFamily: 'Courier New',
+              fontSize: 14.0,
+            ),
+            textAlign: TextAlign.right,
+          ),
+        ],
+      ),
+    );
   }
 }
