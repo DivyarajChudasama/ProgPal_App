16937ccd25877de192278a6f7dc4996393628c23
= <kevalbehera@gmail.com>
2024-02-24 09:12:45 +0530

    java_first_screen

--- lib/screens/java/java_first.dart
+++ lib/screens/java/java_first.dart
@@ -15,40 +15,148 @@ class _JavaFirstState extends State<JavaFirst> {
     'Java is an object-oriented programming language...',
   ];
   int currentIndex = 0;
+  bool showSummary = false;
 
   void changeContent() {
     setState(() {
-      currentIndex = (currentIndex + 1) % contents.length;
+      if (currentIndex < contents.length - 1) {
+        currentIndex++;
+      } else {
+        showSummary = true;
+      }
+    });
+  }
+
+  void reset() {
+    setState(() {
+      currentIndex = 0;
+      showSummary = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Java First'),
+      ),
+      body: showSummary ? _buildSummary() : _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
     return GestureDetector(
       onTap: () {
         changeContent();
       },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Java First'),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/images/java/whatisjava.png',
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 200, // Adjust the height as needed
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        contents[currentIndex],
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      if (currentIndex ==
+                          0) // Show hint to tap the screen to continue for the first content
+                        Text(
+                          'Tap the screen to continue',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            if (currentIndex ==
+                contents.length -
+                    1) // Show Finish button if it's the last content
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showSummary = true;
+                  });
+                },
+                child: Text('Finish'),
+              ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/java/whatisjava.png',
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 20),
-              Text(
-                contents[currentIndex],
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-            ],
+      ),
+    );
+  }
+
+  Widget _buildSummary() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Summary',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-        ),
+          SizedBox(height: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: contents.map((content) {
+              return Text(content);
+            }).toList(),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              reset();
+            },
+            child: Text('Test Yourself'),
+          ),
+        ],
       ),
     );
   }
