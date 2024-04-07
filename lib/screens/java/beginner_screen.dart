c125b84a184c768ecdd0dbe83cb1a4eb584b7aef
= <kevalbehera@gmail.com>
2024-04-07 15:48:12 +0530

    filehandling

--- lib/screens/java/beginner_screen.dart
+++ lib/screens/java/beginner_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:progpal/screens/java/modules/6_filehandle.dart';
 import 'package:progpal/screens/java/programs/applet.dart';
 import 'package:progpal/screens/java/modules/5_oops.dart';
 import 'package:progpal/screens/java/modules/1_introduction.dart';
@@ -267,6 +268,45 @@ class _IndexPageState extends State<IndexPage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => JavaFifth()),
+                ).then((value) {
+                  // Update the state when the user completes the content
+                  setState(() {
+                    _contentCompleted = true;
+                  });
+                });
+              },
+              child: Text(
+                'Start',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.indigo),
+              ),
+            ),
+          ],
+        ),
+        ExpansionTile(
+          title: Row(
+            children: [
+              Text('Java File Handling'),
+              Spacer(),
+              if (_contentCompleted)
+                Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                ),
+            ],
+          ),
+          children: [
+            _buildDropdownMenuItem('If-else Statements'),
+            _buildDropdownMenuItem('Switch Statements'),
+            _buildDropdownMenuItem('Ternary Operator'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => JavaSixth()),
                 ).then((value) {
                   // Update the state when the user completes the content
                   setState(() {
